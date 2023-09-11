b3944aac910f6d0e6d32b727847bfcf58976db64
ImTotem <psb106305@gmail.com>
2023-09-11 11:51:24 +0900

    0주차 - 단어 찾기

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        String sentence = scanner.nextLine();
+        String keyword = scanner.nextLine();
+
+        String answer = "단어 '" +
+                keyword +
+                "' 의 빈도수: " +
+                contains(sentence, keyword);
+
+        System.out.println(answer);
+
+        scanner.close();
+    }
+
+    public static long contains(String sentence, String keyword) {
+
+        List<String> words = new ArrayList<>(
+                Arrays.asList(
+                        sentence.toLowerCase().split(" ")
+                )
+        );
+
+        return words.stream().filter(s -> s.equalsIgnoreCase(keyword)).count();
     }
 }
